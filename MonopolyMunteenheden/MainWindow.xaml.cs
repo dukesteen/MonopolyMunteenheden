929730b7bc6593c9579731afcf7240d084412074
GithubDuke <signaturesenpai@gmail.com>
2019-04-04 12:59:19 +0200

    Installed MaterialDesignThemes and updated .gitignore

--- MonopolyMunteenheden/MainWindow.xaml.cs
+++ MonopolyMunteenheden/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace MonopolyMunteenheden
         {
             InitializeComponent();
         }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
     }
 }
